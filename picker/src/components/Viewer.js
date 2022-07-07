e7aa33b3b577dcb0a02b5db056f26223330f3e48
excess-rose-blossoms <36179404+excess-rose-blossoms@users.noreply.github.com>
2022-07-07 02:22:06 -0700

    Form input submission complete (not yet connected to server)

--- picker/src/components/Viewer.js
+++ picker/src/components/Viewer.js
@@ -11,7 +11,8 @@ import '../css/Viewer.css';
 
 const Viewer = () => {
     const [sceneJSON, setSceneJSON] = useState({});
-    const [selectedItemKey, setSelectedItemKey] = useState(null)
+    const [tentativeItemValue, setTentativeItemValue] = useState(null);
+    const [selectedItemKey, setSelectedItemKey] = useState(null);
 
     useEffect(() => {
         async function fetchData() {
@@ -24,6 +25,39 @@ const Viewer = () => {
 
     const selectItem = (e) => {
         setSelectedItemKey(e.target.innerText);
+    };
+
+    useEffect(() => {
+        setTentativeItemValue(sceneJSON[selectedItemKey]);
+    }, [selectedItemKey, sceneJSON]);
+
+    const handleSubmit = (event) => {
+        let newSceneJSON = {...sceneJSON};
+        newSceneJSON[selectedItemKey] = tentativeItemValue;
+        setSceneJSON(newSceneJSON);
+        event.preventDefault();
+    }
+
+    const onChangeURL = (e) => {
+        setTentativeItemValue({...tentativeItemValue, url: e.target.value});
+    };
+    const onChangeX = (e) => {
+        setTentativeItemValue({...tentativeItemValue, position: {...tentativeItemValue.position, x: parseFloat(e.target.value)}});
+    };
+    const onChangeY = (e) => {
+        setTentativeItemValue({...tentativeItemValue, position: {...tentativeItemValue.position, y: parseFloat(e.target.value)}});
+    };
+    const onChangeZ = (e) => {
+        setTentativeItemValue({...tentativeItemValue, position: {...tentativeItemValue.position, z: parseFloat(e.target.value)}});
+    };
+    const onChangePitch = (e) => {
+        setTentativeItemValue({...tentativeItemValue, rotation: {...tentativeItemValue.rotation, pitch: parseFloat(e.target.value)}});
+    };
+    const onChangeYaw = (e) => {
+        setTentativeItemValue({...tentativeItemValue, rotation: {...tentativeItemValue.rotation, yaw: parseFloat(e.target.value)}});
+    };
+    const onChangeRoll = (e) => {
+        setTentativeItemValue({...tentativeItemValue, rotation: {...tentativeItemValue.rotation, roll: parseFloat(e.target.value)}});
     };
 
     return (
@@ -40,39 +74,40 @@ const Viewer = () => {
             </ListGroup>
         </Card>
         <div>{JSON.stringify(sceneJSON)}</div>
-        {selectedItemKey && <Form style={{ width: '300rem'}}>
+        
+        {tentativeItemValue && <Form onSubmit={handleSubmit} style={{ width: '300rem'}}>
             <Form.Group>
                 <Form.Label>URL</Form.Label>
-                <Form.Control type="text" placeholder={sceneJSON[selectedItemKey].url}/>
+                <Form.Control type="text" onChange={onChangeURL} placeholder={sceneJSON[selectedItemKey].url} value={tentativeItemValue.url}/>
             </Form.Group>
 
             <Row>
                 <Form.Group as={Col}>
                     <Form.Label>x</Form.Label>
-                    <Form.Control type="text" placeholder={sceneJSON[selectedItemKey].position.x}/>
+                    <Form.Control type="number" onChange={onChangeX} placeholder={sceneJSON[selectedItemKey].position.x} value={tentativeItemValue.position.x}/>
                 </Form.Group>
                 <Form.Group as={Col}>
                     <Form.Label>y</Form.Label>
-                    <Form.Control type="text" placeholder={sceneJSON[selectedItemKey].position.y}/>
+                    <Form.Control type="number" onChange={onChangeY} placeholder={sceneJSON[selectedItemKey].position.y} value={tentativeItemValue.position.y}/>
                 </Form.Group>
                 <Form.Group as={Col}>
                     <Form.Label>z</Form.Label>
-                    <Form.Control type="text" placeholder={sceneJSON[selectedItemKey].position.z}/>
+                    <Form.Control type="number" onChange={onChangeZ} placeholder={sceneJSON[selectedItemKey].position.z} value={tentativeItemValue.position.z}/>
                 </Form.Group>
             </Row>
             
             <Row>
                 <Form.Group as={Col}>
                     <Form.Label>Pitch</Form.Label>
-                    <Form.Control type="text" placeholder={sceneJSON[selectedItemKey].rotation.pitch}/>
+                    <Form.Control type="number" onChange={onChangePitch} placeholder={sceneJSON[selectedItemKey].rotation.pitch} value={tentativeItemValue.rotation.pitch}/>
                 </Form.Group>
                 <Form.Group as={Col}>
                     <Form.Label>Yaw</Form.Label>
-                    <Form.Control type="text" placeholder={sceneJSON[selectedItemKey].rotation.yaw}/>
+                    <Form.Control type="number" onChange={onChangeYaw} placeholder={sceneJSON[selectedItemKey].rotation.yaw} value={tentativeItemValue.rotation.yaw}/>
                 </Form.Group>
                 <Form.Group as={Col}>
                     <Form.Label>Roll</Form.Label>
-                    <Form.Control type="text" placeholder={sceneJSON[selectedItemKey].rotation.roll}/>
+                    <Form.Control type="number" onChange={onChangeRoll} placeholder={sceneJSON[selectedItemKey].rotation.roll} value={tentativeItemValue.rotation.roll}/>
                 </Form.Group>
             </Row>   
                 
