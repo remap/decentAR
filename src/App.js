9366577fa792dfbd6f7f93beed6c7036033729ba
excess-rose-blossoms <36179404+excess-rose-blossoms@users.noreply.github.com>
2022-08-09 15:31:18 -0700

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">  
       <Navigator/>
-      <canvas style={{width: "100%", height: "100%"}}id="renderCanvas"></canvas>
+      <div style={{width: "100%", height: "100%", backgroundColor: "blue"}}></div>
     </div>
   );
 }
