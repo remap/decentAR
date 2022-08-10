f3f1315841b03c9f067a72985c854bb9e5c46ef7
excess-rose-blossoms <36179404+excess-rose-blossoms@users.noreply.github.com>
2022-08-09 18:01:36 -0700

    .

--- public/xrcode.js
+++ public/xrcode.js
@@ -10,7 +10,7 @@ let surface, engine, scene, camera
 
 // Given an input JSON file, instantiate and position all models specified in the file relative to the specified origin.
 const parseSceneJSON = (inputScene) => {
-  console.log('bb');
+  console.log('abb');
   if (!inputScene) {
     return;
   }
